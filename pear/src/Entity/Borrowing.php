6252592c626154591ecfde043e11d1860f3ba0aa
EliseJsl <jousselinelise@gmail.com>
2020-04-19 12:32:31 +0200

    emprunter sans contrainte

---
+++ pear/src/Entity/Borrowing.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BorrowingRepository")
+ */
+class Borrowing
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateDebut;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateFin;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Product", inversedBy="borrowings")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $id_product;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="borrowings")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $id_user;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(\DateTimeInterface $dateDebut): self
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->dateFin;
+    }
+
+    public function setDateFin(\DateTimeInterface $dateFin): self
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
+
+    public function getIdProduct(): ?Product
+    {
+        return $this->id_product;
+    }
+
+    public function setIdProduct(?Product $id_product): self
+    {
+        $this->id_product = $id_product;
+
+        return $this;
+    }
+
+    public function getIdUser(): ?User
+    {
+        return $this->id_user;
+    }
+
+    public function setIdUser(?User $id_user): self
+    {
+        $this->id_user = $id_user;
+
+        return $this;
+    }
+
+}
