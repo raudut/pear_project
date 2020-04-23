672d2cdab4856f64f737e177c0cf670d31147f35
unknown <marthe_de_p@hotmail.fr>
2020-04-23 16:27:36 +0200

    Debut des QRcode

--- pear/src/Controller/SecurityController.php
+++ pear/src/Controller/SecurityController.php
@@ -15,9 +15,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
  class SecurityController extends AbstractController
  {
-//     /**
-//      * @Route("/add-client", name="inscription")
-//      */
+
 //     public function registration(Request $request, ObjectManager $manager)
 //     {
 //      $user = new User();
